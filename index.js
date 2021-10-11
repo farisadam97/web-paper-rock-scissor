fd42ee4fe575ab8df0309561bc062fade2545184
farisdam <farisadam97@gmail.com>
2021-10-11 21:30:03 +0700

    remove commented syntax

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-// var session = require('express-session');
-// var bodyParser = require('body-parser');
 var path = require('path');
 const port = 1234
 const database =  require('./public/js/db.json')
@@ -67,45 +65,7 @@ app.post('/login',(req,res,next) => {
             error:true,
             message: error.message
         })
-    }
-    
-
-    
+    }  
 })
-
-// app.post('/status', (req, res, next) => {
-//     try {
-//         const userInput = req.body.username
-//         const passInput = req.body.password
-
-//         const isExist = database.find(data => {
-//             return data.username === userInput
-//         })
-
-//         if (!isExist) {
-//             return res.status(404).json({
-//                 message: 'user tidak ditemukan'
-//             })
-//         }
-
-//         if (isExist.password !== passInput) {
-//             return res.status(401).json({
-//                 message: 'wrong password'
-//             })
-//         }
-
-//         // userInput.map(user => {
-//         //     console.log(user)
-//         // })
-
-//         return res.status(200).json({
-//             message: 'success'
-//         })
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: 'error'
-//         })
-//     }
-// })
 
 app.listen(port)
